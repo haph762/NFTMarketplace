e508a575b5c20de8b0e6429b96820f515dbf2986
Haph762 <phanha762@gmail.com>
2022-10-24 15:36:59 +0700

    done Web UI

--- Web/src/App.js
+++ Web/src/App.js
@@ -1,11 +1,42 @@
 import { Download, Features, SectionWrapper } from "./components";
+import assets from "./assets";
+import styles from "./styles/Global";
 const App = () => {
   return (
     <div>
-      <Download />
-      <SectionWrapper />
+      <SectionWrapper
+        title="You own store of Nifty NFTs. Start selling and growing"
+        description="Buy, store, collect NFTs, exchange & earn crypto. Join 25+ million people using ProNef Marketplace"
+        showBtn
+        mockupImg={assets.homeHero}
+        banner="banner"
+      />
+      <SectionWrapper
+        title="Smart User Interface Marketplace"
+        description="Experience a buttery UI of ProNef NFT Marketplace. Smooth constant colors of a fluent UI design"
+        mockupImg={assets.homeCards}
+        reverse
+      />
       <Features />
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
+      <SectionWrapper
+        title="Development"
+        description="PronNef is built using Expo with runs natively on all users'devices. You can easily get your app into people's hands"
+        mockupImg={assets.feature}
+        reverse
+      />
+      <SectionWrapper
+        title="Create way to showcase the store"
+        description="The app contains two screens. The fist screen lists all NFTs withe the second one show the details of a specific NFT"
+        mockupImg={assets.mockup}
+        banner="banner02"
+      />
+      <Download />
+      <div className="px-4 py-2 justify-center bg-primary flex-col text-center banner04">
+        <p className={`${styles.pText} ${styles.whiteText}`}>
+          Made with love by <span className="bold">Ha Phan</span>
+        </p>
+      </div>
     </div>
   );
 };
