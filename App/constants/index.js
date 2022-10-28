496402a85a7f232a1afa906665d99a1da207203b
Haph762 <phanha762@gmail.com>
2022-10-28 21:01:23 +0700

    change folder Web

--- App/constants/index.js
+++
@@ -1,5 +0,0 @@
-import assets from "./assets";
-import { NFTData } from "./dummy";
-import { COLORS, FONTS, SHADOWS, SIZES } from "./theme";
-
-export { assets, NFTData, COLORS, FONTS, SHADOWS, SIZES };
